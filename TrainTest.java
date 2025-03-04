24892ebcdbcfe36b8e08a941ba85f1640b75adaf
R. Jordan Crouser <jcrouser@smith.edu>
2025-03-04 12:35:31 -0500

    Update TrainTest.java

--- TrainTest.java
+++ TrainTest.java
@@ -1,1 +1,60 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
 
+public class TrainTest {
+
+    // Engine Tests
+    @Test
+    public void testEngineConstructor() {
+        fail();
+    }
+
+    @Test
+    public void testEngineGo() {
+        fail();
+    }
+
+    // Car Tests
+    @Test
+    public void testCarAddPassenger() {
+        fail();
+    }
+
+    @Test
+    public void testCarRemovePassenger() {
+        fail();
+    }
+
+    // Passenger Tests
+    @Test
+    public void testPassengerBoardCarWithSpace() {
+        fail();
+    }
+
+    @Test
+    public void testPassengerBoardCarFull() {
+        fail();
+    }
+
+    // Train Tests
+    @Test
+    public void testTrainConstructor() {
+        fail();
+    }
+
+    @Test
+    public void testTrainPassengerCount() {
+        fail();
+    }
+
+    @Test
+    public void testTrainGetCar() {
+        fail();
+    }
+
+    @Test
+    public void testTrainPrintManifest() {
+        fail();
+    }
+    
+}
